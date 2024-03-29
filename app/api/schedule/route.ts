f9e67d8318e58af0786e82ca1328847082f4e9ea
aeither <36173828+aeither@users.noreply.github.com>
2024-03-30 03:07:27 +0900

    multiple actions

--- app/api/schedule/route.ts
+++ app/api/schedule/route.ts
@@ -12,19 +12,29 @@ export async function POST(request: Request) {
   try {
     const host = request.headers.get('host');
     const body = await request.json();
+    const times = body.times || 1; // Default to 1 if times is not provided
 
     console.log('schedule', body);
 
-    const qstashResponse = await qstashClient.publishJSON({
-      url: `https://${host}/api/qstash`,
-      body,
-      notBefore: body?.timestamp,
-    });
+    const promises = [];
+    for (let i = 0; i < times; i++) {
+      const notBefore = body?.timestamp
+        ? new Date(body.timestamp).getTime() + i * 60 * 1000
+        : null; // Add 1 minute for each subsequent message
+      const qstashResponse = qstashClient.publishJSON({
+        url: `https://${host}/api/qstash`,
+        body,
+        notBefore: body?.timestamp + i * 10, // separate each call by 10 seconds
+      });
+      promises.push(qstashResponse);
+    }
+
+    const qstashResponses = await Promise.all(promises);
 
     const postId = Date.now().toString();
     await redis!.hset(postId, body);
 
-    return NextResponse.json(qstashResponse);
+    return NextResponse.json(qstashResponses);
   } catch (error) {
     console.log(error);
     return NextResponse.json('An error occurred. Please try again later.', {
